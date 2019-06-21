daa27fe405380a6cbc0cb236e9efdff70a20ddef
jrxjsj <juan.sanchez@jaraxa.com>
2019-06-21 11:43:35 +0200

    Avoided call to AudioManager.setMode

--- src/android/com/dooble/audiotoggle/AudioTogglePlugin.java
+++ src/android/com/dooble/audiotoggle/AudioTogglePlugin.java
@@ -33,19 +33,19 @@ public class AudioTogglePlugin extends CordovaPlugin {
 	    	(AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 	    
 	    if (mode.equals("earpiece")) {
-	    	audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
+	    	//audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
 	    	audioManager.setSpeakerphoneOn(false);
 	        return true;
 	    } else if (mode.equals("speaker")) {        
-	    	audioManager.setMode(AudioManager.STREAM_MUSIC);
+	    	//audioManager.setMode(AudioManager.STREAM_MUSIC);
 	    	audioManager.setSpeakerphoneOn(true);
 	        return true;
 	    } else if (mode.equals("ringtone")) {        
-	    	audioManager.setMode(AudioManager.MODE_RINGTONE);
+	    	//audioManager.setMode(AudioManager.MODE_RINGTONE);
 	    	audioManager.setSpeakerphoneOn(false);
 	        return true; 
 	    } else if (mode.equals("normal")) {        
-	    	audioManager.setMode(AudioManager.MODE_NORMAL);
+	    	//audioManager.setMode(AudioManager.MODE_NORMAL);
 	    	audioManager.setSpeakerphoneOn(true);
 	        return true;
 	    }
